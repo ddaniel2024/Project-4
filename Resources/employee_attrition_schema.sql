e0213c2141fd11a09b916d1220378463e678f614
ddaniel2024 <daniel97daniel@gmail.com>
2024-07-16 22:23:28 +0100

    added postgres password to gitignore

---
+++ Resources/employee_attrition_schema.sql
@@ -0,0 +1,41 @@
+DROP TABLE attrition;
+
+CREATE TABLE attrition (
+    Age INT,
+    Attrition VARCHAR(3),
+    BusinessTravel VARCHAR(30),
+    DailyRate INT,
+    Department VARCHAR(30),
+    DistanceFromHome INT,
+    Education INT,
+    EducationField VARCHAR(30),
+    EmployeeCount INT,
+    EmployeeNumber INT,
+    EnvironmentSatisfaction INT,
+    Gender VARCHAR(6),
+    HourlyRate INT,
+    JobInvolvement INT,
+    JobLevel INT,
+    JobRole VARCHAR(30),
+    JobSatisfaction INT,
+    MaritalStatus VARCHAR(10),
+    MonthlyIncome INT,
+    MonthlyRate INT,
+    NumCompaniesWorked INT,
+    Over18 VARCHAR(1),
+    OverTime VARCHAR(3),
+    PercentSalaryHike INT,
+    PerformanceRating INT,
+    RelationshipSatisfaction INT,
+    StandardHours INT,
+    StockOptionLevel INT,
+    TotalWorkingYears INT,
+    TrainingTimesLastYear INT,
+    workLifeBalance INT,
+    YearsAtCompany INT,
+    YearsInCurrentRole INT,
+    YearsSinceLastPromotion INT,
+    YearsWithCurrManager INT
+);
+
+SELECT * FROM attrition;
